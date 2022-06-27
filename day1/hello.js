810b1a02aacea12fc339319cbfa3b72841610469
congs00n <ninxtxn@gmail.com>
2022-06-27 09:18:24 +0900

    from air

---
+++ day1/hello.js
@@ -0,0 +1,7 @@
+let colors = require ('colors')
+
+// node.js 크롬에서 오픈소스로 사용할 수 있는 v8에디션이 설정되어 있고, npm명령어를 이용하면 표쥰의 명령어가 아닌 많은 사람들이 만든 아이디어를 사용할 수 있다. 
+console.log(colors.rainbow('Hello NodeJS!!!'));
+console.log(colors.green('Hello NodeJS!!!'));
+
+
